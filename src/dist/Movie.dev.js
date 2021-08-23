7ecf524033da8685f959e0ec0114acaad20a6ad8
saehee-park <hee00315@gmail.com>
2021-08-24 01:16:20 +0900

    Creating first React app

---
+++ src/dist/Movie.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+var _propTypes = _interopRequireDefault(require("prop-types"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+//# sourceMappingURL=Movie.dev.js.map
